484e31a9a464ccb760bf1670fef47877ba8a01f0
Edmar Pereira <edmar.pereira80@gmail.com>
2026-03-14 12:28:56 -0300

    fix

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import Main from './pages/main';
 import Graphic from './pages/graphic';
@@ -14,12 +12,6 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 export default function Router() {
   const isLoggedIn = !!localStorage.getItem('accessToken'); // verifica token
-
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log('Path atual:', location.pathname);
-  }, [location]);
 
   return (
     <Routes>
